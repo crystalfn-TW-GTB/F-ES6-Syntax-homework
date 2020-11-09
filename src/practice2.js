49307604e2cdd73d89e5c5935e5bd06e631fc1df
crystalfn <969274162@qq.com>
2020-11-10 00:22:03 +0800

    [fangni] refactor: refactor practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,13 +1,9 @@
 const inject = (items, sections) => {
-  const resultArray = [];
-  items.forEach((item, index) => {
-    sections.forEach(section => {
-      if (section.index === index) {
-        resultArray.push(section.content);
-      }
-    });
-    resultArray.push(item);
-  });
+  const resultArray = [...items];
+  sections.sort((item1, item2) => item2.index - item1.index);
+  sections.forEach((item) => {
+    resultArray.splice(item.index, 0, item.content);
+  })
   return resultArray;
 }
 export { inject };
