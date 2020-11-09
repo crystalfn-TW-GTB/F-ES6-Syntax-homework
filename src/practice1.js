5ebe8792640d920a418b71d81e2e0e78474ea54a
crystalfn <969274162@qq.com>
2020-11-09 23:22:24 +0800

    [fangni] feat: implement practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
+const parseData = (input) => {
+  const { data, column } = input;
+  const keys = column.map(item => item.name);
+  return data.map((item) => createObject(keys, item));
+};
+
+function createObject(keys, values) {
+  const object = {};
+  keys.forEach((key, index) => {
+    object[key] = values[index];
+  });
+  return object;
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
+const inject = (items, sections) => {
+  const resultArray = [];
+  items.forEach((item, index) => {
+    sections.forEach(section => {
+      if (section.index === index) {
+        resultArray.push(section.content);
+      }
+    });
+    resultArray.push(item);
+  });
+  return resultArray;
+}
 export { inject };
